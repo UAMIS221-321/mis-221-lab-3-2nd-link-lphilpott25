e4938340e8a27c53954df80e529e47a93cbe8e6c
Liam Philpott <lphilpott@crimson.ua.edu>
2024-09-14 23:56:23 -0500

    lab3

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿string enjoymentLevel = GetEnjoymentLevel();
+string stadium = GetStadiumRecommendation(enjoymentLevel);
+string game = GetGameRecommendation(stadium);
+DisplayStadiumDetails(stadium, game);
+
+
+static string GetEnjoymentLevel()
+{
+    System.Console.WriteLine("Prompt the user for the enjoyment level of the game they want to atend");
+    return Console.ReadLine().ToUpper();
+}
+
+static string GetStadiumRecommendation(string enjoymentLevel)
+{
+    if (enjoymentLevel == "BORING")
+    {
+        return "Neyland Stadium";
+    }
+    if (enjoymentLevel == "AVERAGE")
+    {
+        return "Jordan-Hare Stadium";
+    }
+    if (enjoymentLevel == "FUN")
+    {
+        return "Tiger Stadium";
+    }
+    if (enjoymentLevel == "EPIC")
+    {
+        return "Saban Field at Bryant-Denny Stadium";
+    }
+    else
+    {
+        return "";
+    }
+}
+
+static string GetGameRecommendation(string stadium)
+{
+    if(stadium == "Neyland Stadium")
+    {
+        return "Kent State";
+    }
+    if(stadium == "Jordan Hare Stadium")
+    {
+        return "Kentucky";
+    }
+    if(stadium == "Tiger Stadium")
+    {
+        return "Alabama";
+    }
+    if(stadium == "Epic")
+    {
+        return "Auburn";
+    }
+    else{
+        return "";
+    }
+}
+
+static void DisplayStadiumDetails( string stadium, string game )
+{
+    System.Console.WriteLine(stadium + " " + game); 
+}
+
